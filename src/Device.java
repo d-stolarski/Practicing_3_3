200227dd96cf8958f881d37f8e7412c5996e694d
Daniel Stolarski <d.stolarski@o2.pl>
2017-11-29 20:40:07 +0100

    Final Commit - class Device modified

--- src/Device.java
+++ src/Device.java
@@ -1,2 +1,22 @@
 public class Device {
+    String code;
+    String producer;
+    String type;
+    double price;
+
+    Device(String code, String producer, String type) {
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+    }
+
+    Device(String code, String producer, String type, double price) {
+        this(code, producer, type);
+        this.price = price;
+    }
+
+    String getInfo() {
+        String info = "Kod " + code + ", producent: " + producer + ", typ " + type + ", cena " + price;
+        return info;
+    }
 }
